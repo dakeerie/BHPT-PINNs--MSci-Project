82a78db3ba00c62485c9a3774a304c8f952faca4
dakeerie <daniel@keerie.co.uk>
2026-09-16 22:40:53 +0100

    Small print change

--- GBF/PINNGBFWS.py
+++ GBF/PINNGBFWS.py
@@ -30,6 +30,10 @@ mode = args.mode
 #Frequency schedule from high to low for continuation
 omega_schedule = np.linspace(args.omega_start, args.omega_final, args.num_steps)
 
+print(f"Initialising training for omega = {omega_schedule}")
+
+print("Not safe to train yet!")
+# raise
 # mode = 2
 # omega = 0.3
 
